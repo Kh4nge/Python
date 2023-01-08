235c1d2387b516f85da501f68eda1d20fb776bcb
root <root@kali.home>
2023-01-08 18:16:30 +0100

    test2

--- menu-1.py
+++ menu-1.py
@@ -2,12 +2,12 @@ from tkinter import Tk, mainloop, TOP
 from tkinter.ttk import Button
 import subprocess as subprocess
 
-screen = TK()
+screen = Tk()
 screen.title("Main Page")
 screen.geometry("200x170")
-screen.configure("#463E3F")
+screen.configure(bg="#463E3F")
 
-b1 = Button(screen, text="REBOOT", command=lambda: sub.call('./command.sh'))
+b1 = Button(screen, text="REBOOT", command=lambda: sub.call('./reboot.sh'))
 b1.pack(side=TOP, pady=5)
 
 screen.mainloop()
