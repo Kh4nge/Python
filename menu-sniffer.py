0684c81b10e460d21a2ee42a1d1ede5dbea21cef
Kh4nge <kh4nge@proton.me>
2023-01-10 17:57:48 +0100

    update10

--- menu-sniffer.py
+++ menu-sniffer.py
@@ -15,20 +15,24 @@ xterm_frame.pack(fill=tk.BOTH, expand=True)
 frame1 = tk.Frame(root)
 frame1.pack()
 
+xterm_frame_id = xterm_frame.winfo_id()
+xterm_frame.place(x=120, y=0, width=360, height=320)
+
+def ls():
+    xterm_frame.bind('<B1-Motion>', motion)
+
 b1 = tk.Button(root, text="CLOSE", bg="black", fg="white", command=root.destroy, activeforeground="black", activebackground="red")
 b1.place(x=0, y=0, width=120, height=80)
 
-b2 = tk.Button(root, text="Start", bg="black", fg="white", command=root.destroy, activeforeground="black", activebackground="red")
+b2 = tk.Button(root, text="1. New-File", bg="black", fg="white", activeforeground="black", activebackground="red")
 b2.place(x=0, y=80, width=120, height=80)
+#command=subprocess.Popen(["xterm", "-into", str(xterm_frame_id), "-e", "ls"]
 
-b3 = tk.Button(root, text="Stop", bg="black", fg="white", command=root.destroy, activeforeground="black", activebackground="red")
+b3 = tk.Button(root, text="2. Start", bg="black", fg="white", command=root.destroy, activeforeground="black", activebackground="red")
 b3.place(x=0, y=160, width=120, height=80)
 
-b4 = tk.Button(root, text="Altro", bg="black", fg="white", command=root.destroy, activeforeground="black", activebackground="red")
+b4 = tk.Button(root, text="Stop", bg="black", fg="white", command=root.destroy, activeforeground="black", activebackground="red")
 b4.place(x=0, y=240, width=120, height=80)
-
-xterm_frame_id = xterm_frame.winfo_id()
-xterm_frame.place(x=120, y=0, width=360, height=320)
 
 try:
     p = subprocess.Popen(
