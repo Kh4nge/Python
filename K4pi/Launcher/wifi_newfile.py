17ecfda1bb69a3fb70a501fa40a18be3a582d402
root <root@kali.home>
2023-01-11 21:29:48 +0100

    update1011

--- K4pi/Launcher/wifi_newfile.py
+++ K4pi/Launcher/wifi_newfile.py
@@ -17,22 +17,8 @@ def process(x, cmd, text):
     x.insert("1.0", output.decode())
     x.config(state=DISABLED)
     
-"""
-def process_sudo(x, cmd):
-    password = "Toldo93xd2"
-    cmd2 = str(cmd)
-    command = "sudo" + cmd2
-    process = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    output, error = process.communicate(input=password.encode() + b"\n")
-    x.config(state=NORMAL)
-    x.delete("1.0", END)
-    x.insert("1.0", output.decode())
-    x.insert("1.0", result.stdout.decode())
-    x.insert("1.0", result.stderr.decode())
-    x.config(state=DISABLED)
-"""
-
 def newfile_run(y, time):
+    #Create a Folder
     user = getpass.getuser()
     folder_path = ("/home/" + user + "/dumpfile")
     if not os.path.exists(folder_path):
@@ -42,31 +28,16 @@ def newfile_run(y, time):
         print(f"K4pi >>> The folder {folder_path} has been created.")
         text = "K4pi >>> The folder " + folder_path + " has been created."
         process(y, "2>/dev/null", text)
-
-
+        #Create a File
         text = "K4pi >>> File " + time + ".txt has been created"
-        process(y, "touch /home/" + user + "/dumpfile/" + time + ".txt", text)
-
+        dumpfile = time + ".pcapng"
+        process(y, "touch /home/" + user + "/dumpfile/" + dumpfile, text)
         text = "---------------------------------"
         process(y, "2>/dev/null", text)
-
-        #subprocess.call(['echo', 'Toldo93xd2', '|', 'sudo', 'touch', folder_path])
-        #subprocess.call(['echo', 'Toldo93xd2', '|', 'sudo', 'chmod', '777', folder_path])
-        #print(f"K4pi >>> The file dump.txt has been created.")
     else:
         print(f"K4pi >>> The folder {folder_path} already exists")
         text = "K4pi >>> The folder " + folder_path + " already exists"
-        process(y, "2>/dev/null", text)
-    
-    
-    
-    #print (folder_path)
-    #path2 = "touch " + folder_path + "/dump.txt"
-    #p#rint (path2)
-    #test = process(y, "/usr/bin/touch" + folder_path + "/dump.txt", "Creazione file touch")
-    #process(y, "chmod " + " 777 " + folder_path + "/dump.txt")
-    #process(y, folder_path + "ls")
-    
+        process(y, "2>/dev/null", text)   
 
 def newfile_clear(y):
     process(y, "cat wifi_start.py")
--- K4pi/Launcher/wifi_start.py
+++ K4pi/Launcher/wifi_start.py
@@ -4,3 +4,24 @@ import subprocess
 import tkinter as tk
 from tkinter import *
 import os
+import wifi_newfile
+
+"""
+Comandi:
+sudo systemctl stop NetworkManager.service
+sudo systemctl stop wpa_supplicant.service
+iwconfig
+sudo hcxdumptool -i wlan1 -o dumpfile.pcapng --active_beacon --enable_status=15
+sudo systemctl start NetworkManager.service
+sudo systemctl start wpa_supplicant.service 
+"""
+
+def start_run(y):
+    text = "K4pi >>> Stop NetworkManager.service"
+    wifi_newfile.process(y, "systemctl stop NetworkManager.service", text)
+    text = "K4pi >>> Stop wpa_supplicant.service"
+    wifi_newfile.process(y, "systemctl stop wpa_supplicant.service", text)
+    text = "---------------------------------"
+    wifi_newfile.process(y, "2>/dev/null", text)
+    text = "K4pi >>> START SCANNER"
+    wifi_newfile.process(y, "/home/Kh4nge/Script/GBLauncher/Python/K4pi/hcxdumptool/hcxdumptool -i wlan1 -o " + wifi_newfile.dumpfile + " --active_beacon --enable_status=15", text)
