4d3c29c8c5ce17c3f65792c43eda7305b298bc36
Kh4nge <kh4nge@proton.me>
2023-01-14 15:15:40 +0100

    update1401

--- K4pi/Launcher/menu-1.py
+++ K4pi/Launcher/menu-1.py
@@ -5,32 +5,52 @@ import subprocess as sub
 import tkinter as tk
 from subprocess import Popen
 import os
+from PIL import Image, ImageTk
 
-#Configuration Windows
+#----------------------------------
+# Configuration Windows and Image
+#----------------------------------
+
 root = tk.Tk()
 root.title("Main Page")
 root.geometry("480x320")
 root.configure(bg="#463E3F")
-root.attributes("-fullscreen", True)
+#root.attributes("-fullscreen", True)
 
-#Configuration buttons and objects
+image = Image.open("desk1.jpg")
+photo = ImageTk.PhotoImage(image)
+label = Label(root, image=photo)
+label.configure(background='black')
+label.place(x=0, y=0, width=480, height=320)
+
+#----------------------------------
+# Configuration buttons and objects
+#----------------------------------
+
 b1 = tk.Button(root, text="CLOSE", bg="black", fg="white", command=root.destroy, activeforeground="black", activebackground="red")
-b1.place(x=0, y=0, width=120, height=80)
-
-l1 = tk.Label(root, text="GB - Launcher for Rapsberry", bg="black", fg="white")
-l1.place(x=120, y=0, width=360, height=80)
+b1.config(font=("Encode Sans SemiExpanded", 18))
+b1.configure(highlightthickness=0)
+b1.place(x=4, y=4, width=114, height=74)
 
 b2 = tk.Button(root, text="WiFi-Sniffer", bg="black", fg="white", command=lambda: os.system('python3 menu-sniffer.py'), activeforeground="black", activebackground="#00ff5f")
-b2.place(x=0, y=80, width=240, height=160)
+b2.config(font=("Encode Sans SemiExpanded", 20))
+b2.configure(highlightthickness=0)
+b2.place(x=4, y=82, width=234, height=116)
 
-b3 = tk.Button(root, text="Altro", bg="black", fg="white", activeforeground="black", activebackground="#00ff5f")
-b3.place(x=240, y=80, width=240, height=160)
+b3 = tk.Button(root, text="RasAP", bg="black", fg="white", activeforeground="black", activebackground="#00ff5f")
+b3.config(font=("Encode Sans SemiExpanded", 25))
+b3.configure(highlightthickness=0)
+b3.place(x=242, y=82, width=234, height=116)
 
-b4 = tk.Button(root, text="Altro", bg="black", fg="white", activeforeground="black", activebackground="#00ff5f")
-b4.place(x=0, y=200, width=240, height=150)
+b4 = tk.Button(root, text="OpenVAS", bg="black", fg="white", activeforeground="black", activebackground="#00ff5f")
+b4.config(font=("Encode Sans SemiExpanded", 25))
+b4.configure(highlightthickness=0)
+b4.place(x=4, y=202, width=234, height=114)
 
-b5 = tk.Button(root, text="Altro", bg="black", fg="white", activeforeground="black", activebackground="#00ff5f")
-b5.place(x=240, y=200, width=240, height=150)
+b5 = tk.Button(root, text="Kismet", bg="black", fg="white", activeforeground="black", activebackground="#00ff5f")
+b5.config(font=("Encode Sans SemiExpanded", 25))
+b5.configure(highlightthickness=0)
+b5.place(x=242, y=202, width=234, height=114)
 
 #Loop for not close the window
 root.mainloop()
--- K4pi/Launcher/menu-sniffer.py
+++ K4pi/Launcher/menu-sniffer.py
@@ -6,9 +6,15 @@ import tkinter as tk
 from tkinter import *
 import os
 from datetime import datetime
+from PIL import Image, ImageTk
 import wifi_newfile
 import wifi_start
 import wifi_stop
+import wifi_newfile
+
+#------------------------------------
+# Configuration Windows, Image, Time
+#------------------------------------
 
 root = tk.Tk()
 root.title("WiFi Page")
@@ -16,31 +22,57 @@ root.geometry("480x320")
 root.configure(bg="#463E3F")
 #root.attributes("-fullscreen", True)
 
+image = Image.open("desk2.jpg")
+photo = ImageTk.PhotoImage(image)
+label = Label(root, image=photo)
+label.configure(background='black')
+label.place(x=0, y=0, width=480, height=320)
+
 current_time = datetime.now()
 time = current_time.strftime("%m%d-%H%M%S")
 print (time)
 
-#Functions for button
+#--------------------------------------
+# Button Function's
+#--------------------------------------
+
 def b2_command():
     wifi_newfile.newfile_run(t1, time)
 def b3_command():
     wifi_start.start_run(t1)
 
-# Create output text Campo
+#--------------------------------------
+# Configuration buttons and Text label
+#--------------------------------------
+
 t1 = Text(root, bg="black", fg="white")
-t1.place(x=120, y=0, width=360, height=320)
+t1.place(x=122, y=4, width=354, height=312)
 
 b1 = tk.Button(root, text="CLOSE", bg="black", fg="white", command=root.destroy, activeforeground="black", activebackground="red")
-b1.place(x=0, y=0, width=120, height=80)
+b1.config(font=("Encode Sans SemiExpanded", 18))
+b1.configure(highlightthickness=0)
+b1.place(x=4, y=4, width=114, height=74)
     
-b2 = tk.Button(root, text="1. New-File", bg="black", fg="white", command=b2_command, activeforeground="black", activebackground="#00ff5f")
-b2.place(x=0, y=80, width=120, height=80)
+b2 = tk.Button(root, text="New-File", bg="black", fg="white", command=b2_command, activeforeground="black", activebackground="#00ff5f")
+b2.config(font=("Encode Sans SemiExpanded", 15))
+b2.configure(highlightthickness=0)
+b2.place(x=4, y=82, width=114, height=76)
 
-b3 = tk.Button(root, text="2. Start", bg="black", fg="white", command=b3_command, activeforeground="black", activebackground="#00ff5f")
-b3.place(x=0, y=160, width=120, height=80)
+b3 = tk.Button(root, text="Start", bg="black", fg="white", command=b3_command, activeforeground="black", activebackground="#00ff5f")
+b3.config(font=("Encode Sans SemiExpanded", 20))
+b3.configure(highlightthickness=0)
+b3.place(x=4, y=162, width=114, height=76)
 
 b4 = tk.Button(root, text="Stop", bg="black", fg="white", command=root.destroy, activeforeground="black", activebackground="#00ff5f")
-b4.place(x=0, y=240, width=120, height=80)
+b4.config(font=("Encode Sans SemiExpanded", 20))
+b4.configure(highlightthickness=0)
+b4.place(x=4, y=242, width=114, height=74)
+
+#        --------------------------------------------- row K4pi >>> for terminal
+text1 = "K4pi >>> Please create a new file.           "
+wifi_newfile.process(t1, "2>/dev/null", text1)
+text2 = "                                            "
+wifi_newfile.process(t1, "2>/dev/null", text2) 
 
 #Loop for not close the window
 root.mainloop()
