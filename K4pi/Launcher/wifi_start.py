cd6ba764a654db4e09742c30ab36348d57c19f0b
Kh4nge <kh4nge@proton.me>
2023-01-15 21:11:34 +0100

    update1501

--- K4pi/Launcher/wifi_start.py
+++ K4pi/Launcher/wifi_start.py
@@ -46,10 +46,6 @@ def start_run(y):
     text.insert(INSERT, "Process completed!")
     terminal.mainloop()
     """
-    root2 = tk.Tk()
-    new_window = tk.Toplevel(root2)
-    new_window.geometry("354x312+122+4")
-    new_window.title("xterm")
-    command = "xterm -hold -e sudo /home/Kh4nge/Script/GBLauncher/Python/K4pi/hcxdumptool/hcxdumptool -i wlan1 -o test1.pcapng --active_beacon --enable_status=15"
+    command = "xterm -hold -geometry 354x312+122+4 -border 0 -e sudo /home/Kh4nge/Script/GBLauncher/Python/K4pi/hcxdumptool/hcxdumptool -i wlan1 -o test1.pcapng --active_beacon --enable_status=15"
     process = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True, start_new_session=True, close_fds=True, cwd=None, env=None, shell=True)
 
