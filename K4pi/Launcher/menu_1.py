b6d3f07b4a906362f13f2daaac36bc008f6f4132
Kh4nge <kh4nge@proton.me>
2023-01-25 15:36:07 +0100

    update2501

--- K4pi/Launcher/menu_1.py
+++ K4pi/Launcher/menu_1.py
@@ -23,6 +23,24 @@ label = Label(root, image=photo)
 label.configure(background='black')
 label.place(x=0, y=0, width=480, height=320)
 
+def start():
+    timeset = datetime.now()
+    current_time = timeset.strftime("%m%d%H%M%S")
+    folder_name = "/home/Kh4nge/dumpfile/"
+    os.makedirs(folder_name, 0o777, exist_ok=True)
+    file_name = current_time + ".pcapng"
+    file_path = os.path.join(folder_name, file_name)
+    with open(file_path, "w") as f:
+        pass
+    os.chmod(file_path, 0o777)
+    os.system("sudo systemctl stop NetworkManager.service")
+    os.system("sudo systemctl stop wpa_supplicant.service")
+    #output = os.popen("sudo hcxdumptool -i wlan1 -o "+ file_path +" --active_beacon --enable_status=15;bash").read()
+    #cmd = "sudo xterm -hold -e 'sudo hcxdumptool -i wlan1 -o " + file_path + " --active_beacon --enable_status=15;bash'"
+    #cmd = "sudo hcxdumptool -i wlan0 -o "+ file_path +" --active_beacon --enable_status=15;bash"
+    cmd = "sudo terminator -e 'sudo hcxdumptool -i wlan1 -o "+ file_path +" --active_beacon --enable_status=15;bash'"
+    os.system(cmd)
+
 #----------------------------------
 # Configuration buttons and objects
 #----------------------------------
@@ -32,7 +50,7 @@ b1.config(font=("Encode Sans SemiExpanded", 18))
 b1.configure(highlightthickness=0)
 b1.place(x=4, y=4, width=114, height=74)
 
-b2 = tk.Button(root, text="WiFi-Sniffer", bg="black", fg="white", command=lambda: os.system('python3 menu_sniffer.py'), activeforeground="black", activebackground="#00ff5f")
+b2 = tk.Button(root, text="WiFi-Sniffer", bg="black", fg="white", command=lambda: start(), activeforeground="black", activebackground="#00ff5f")
 b2.config(font=("Encode Sans SemiExpanded", 20))
 b2.configure(highlightthickness=0)
 b2.place(x=4, y=82, width=234, height=116)
---
+++ K4pi/Launcher/sniffer.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+data_ora = $(date +"%m%d%y%H%M%S")
+echo (data_ora)
